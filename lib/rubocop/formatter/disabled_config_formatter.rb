c50da30f550d19fd0a0d6f01f11f3b832c2c3852
Maxim Krizhanovsky <maxim.krizhanovski@toptal.com>
2018-09-22 08:42:49 +0300

    fixup! Fix build

--- lib/rubocop/formatter/disabled_config_formatter.rb
+++ lib/rubocop/formatter/disabled_config_formatter.rb
@@ -117,7 +117,7 @@ module RuboCop
       def cop_config_params(default_cfg, cfg)
         default_cfg.keys -
           %w[Description StyleGuide Reference Enabled Exclude Safe
-            SafeAutoCorrect VersionAdded VersionChanged VersionRemoved] -
+             SafeAutoCorrect VersionAdded VersionChanged VersionRemoved] -
           cfg.keys
       end
 
